74ff95b9abf065e0660d003e7d854915de08b3b8
Martin <martinthelindberg@gmail.com>
2020-05-19 17:35:50 -0700

    New limits bb!!

--- functions/limit-authors.php
+++ functions/limit-authors.php
@@ -6,6 +6,10 @@
  * 2. Remove admin panel menu items (on the left)
  * 3. Remove admin bar menu items (on the top)
  * 4. Remove unecessary input fields on Profile page
+ * 5. Disable dragging of ACF groups (need to edit to include flexible content sections)
+ * 6. Limit published posts to 3
+ * 7. Remove quick edit feature
+ * 8. Limit media library access
  */
 $user = wp_get_current_user();
 if (in_array('author', (array) $user->roles)) {
@@ -62,7 +66,7 @@ if (in_array('author', (array) $user->roles)) {
     $wp_admin_bar->remove_node('customize');
   }
 
-  // 4.
+  // 4. & 5.
   add_action('admin_footer', 'portshowlio20_remove_profile_fields');
   function portshowlio20_remove_profile_fields()
   {
@@ -72,17 +76,16 @@ if (in_array('author', (array) $user->roles)) {
         $('#your-profile').children('h2').remove(); // All headers
         $('input#rich_editing').closest('table').remove() // Personal Options content
         $('input#user_login').closest('tr').remove(); // Username
-        // $('input#nickname').closest('tr').remove(); // Nickname (required)
         $('input#nickname').closest('tr').css("display", "none"); // Nickname (required)
         $('select#display_name').closest('tr').remove(); // Display my name as...
         $('input#url').closest('tr').remove(); // Website (will handle with ACF)
         $('textarea#description').closest('table').remove(); // About
 
         // INSERT header for ACF section
-        $('<h2>Student Info</h2>').insertBefore( $('.acf-field').closest('table') );
+        $('<h1>Student Info</h1>').insertBefore( $('.acf-field').closest('table') );
 
         // Wrap sections TBD...
-        $('#your-profile').children('table:lt(3)').wrapAll( "<div class='new'></div>" );;
+        $('#your-profile').children('table:lt(3)').wrapAll( "<div id='wordpress-prof-info'></div>" );;
 
       });
     </script>
@@ -92,7 +95,82 @@ if (in_array('author', (array) $user->roles)) {
       .acf-postbox .ui-sortable-handle {
         pointer-events: none;
       }
+
+      /* remove h2s with "student info" */
+      #profile-page .acf-input h1,
+      .acf-input h2 {
+        display: none;
+      }
+
+      #wordpress-prof-info {
+        padding: 2rem;
+        border: 3px solid gray;
+        border-radius: 5px;
+        margin-bottom: 2rem;
+      }
     </style>
     <?php
+  }
+
+  // 6.(THERE'S A HOLE HERE: IF A USER HAS 3 PROJECTS, SETS ONE TO DRAFT, THEN MAKES AND PUBLISHES A NEW PROJECT, THEN RETURNS TO THE DRAFT PROJECT, THEY CAN SET THAT TO PUBLISH AND GET 4 PROJECTS)
+  add_action('load-post-new.php', 'limit_add_new_post');
+  function limit_add_new_post()
+  {
+    global $typenow;
+
+    # Not our post type, bail out
+    if ('projects' !== $typenow) {
+      return;
+    }
+
+    $current_user = wp_get_current_user();
+    $user_id = $current_user->ID;
+
+    # Grab all our CPT, adjust the status as needed
+    $total = get_posts([
+      'author' => $user_id,
+      'post_type' => 'projects',
+      'post_status' => 'publish',
+    ]);
+
+    # Condition match, block new post
+    if ($total && count($total) >= 3) {
+      wp_die(
+        "Sorry, maximum number of published projects reached!",
+        'Maximum reached',
+        [
+          'response' => 500,
+          'back_link' => true,
+        ]
+      );
+    }
+  }
+
+  // 7.
+  add_filter('post_row_actions', 'remove_quick_edit', 10, 1);
+  function remove_quick_edit($actions)
+  {
+    unset($actions['inline hide-if-no-js']);
+    return $actions;
+  }
+
+  // 8.
+  // Limit media library access... questionable..
+  add_filter(
+    'ajax_query_attachments_args',
+    'wpb_show_current_user_attachments'
+  );
+
+  function wpb_show_current_user_attachments($query)
+  {
+    $user_id = get_current_user_id();
+    if (
+      $user_id &&
+      !current_user_can('activate_plugins') &&
+      !current_user_can('edit_others_posts')
+    ) {
+      $query['author'] = $user_id;
+    }
+    return $query;
   }
 }
--- functions/limit-focus-to-four.php
+++ functions/limit-focus-to-four.php
@@ -4,6 +4,7 @@
  * Validate the categories input incase they add a custom category at the end!
  */
 add_filter('acf/validate_value/name=project_tags', 'acf_focus_to_four', 10, 4);
+add_filter('acf/validate_value/name=your_roles', 'acf_focus_to_four', 10, 4);
 add_filter('acf/validate_value/name=roles', 'acf_focus_to_four', 10, 4);
 add_filter('acf/validate_value/name=focus', 'acf_focus_to_four', 10, 4);
 function acf_focus_to_four($valid, $value, $field, $input_name)
