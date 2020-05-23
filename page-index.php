a2a6fd324ab26b3c6d34e1a1030213a6bdadff27
Martin <martinthelindberg@gmail.com>
2020-05-23 12:58:34 -0700

    Brought back the grid of projects/students

---
+++ page-index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The page-index template file
+ *
+ * This basically overwrites the "Index" page that is/was created in the WP dashboard.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package portshowlio20
+ */
+
+get_header(); ?>
+
+  <main id="index" class="site-main">
+
+  <?php get_template_part('components/index/filter', 'form'); ?>
+  <?php get_template_part('components/index/filter', 'response'); ?>
+
+	</main><!-- #main -->
+
+<?php get_footer();
