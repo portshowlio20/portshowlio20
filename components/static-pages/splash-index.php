327f1684618a675d2c4eca8a30d82027f9058d12
Martin <martinthelindberg@gmail.com>
2020-05-24 10:08:35 -0700

    New MailChimp form

--- components/static-pages/splash-index.php
+++ components/static-pages/splash-index.php
@@ -128,91 +128,32 @@
                 </p>
                 <!-- Begin Mailchimp Signup Form -->
                 <div id="mc_embed_signup">
-                  <form
-                    action="https://seattlecentralcreativeacademy.us10.list-manage.com/subscribe/post?u=29d9200913b86a01277bcfece&amp;id=08c5259d3b"
-                    method="post"
-                    id="mc-embedded-subscribe-form"
-                    name="mc-embedded-subscribe-form"
-                    class="validate"
-                    target="_blank"
-                    novalidate
-                  >
+                  <form action="https://seattlecentralcreativeacademy.us10.list-manage.com/subscribe/post?u=29d9200913b86a01277bcfece&amp;id=5804700c08" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                     <div id="mc_embed_signup_scroll">
+
                       <div class="mc-field-group">
-                        <label class="caption" for="mce-FNAME"
-                          >First Name <span class="optional">(optional)</span>
-                        </label>
-                        <input
-                          type="text"
-                          value=""
-                          name="FNAME"
-                          class=""
-                          id="mce-FNAME"
-                        />
+                        <label class="caption" for="mce-FNAME">First Name <span>(optional)</span></label>
+                        <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
                       </div>
+
                       <div class="mc-field-group">
-                        <label class="caption" for="mce-EMAIL"
-                          >Email Address
-                        </label>
-                        <input
-                          type="email"
-                          value=""
-                          name="EMAIL"
-                          class="required email"
-                          id="mce-EMAIL"
-                        />
+                        <label class="caption" for="mce-EMAIL">Email Address </label>
+                        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
                       </div>
+
                       <div id="mce-responses" class="clear">
-                        <div
-                          class="response caption"
-                          id="mce-error-response"
-                          style="display: none;"
-                        ></div>
-                        <div
-                          class="response"
-                          id="mce-success-response"
-                          style="display: none;"
-                        ></div>
+                        <div class="response" id="mce-error-response" style="display:none"></div>
+                        <div class="response" id="mce-success-response" style="display:none"></div>
                       </div>
+
                       <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                      <div
-                        style="position: absolute; left: -5000px;"
-                        aria-hidden="true"
-                      >
-                        <input
-                          type="text"
-                          name="b_29d9200913b86a01277bcfece_08c5259d3b"
-                          tabindex="-1"
-                          value=""
-                        />
-                      </div>
+                      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_29d9200913b86a01277bcfece_5804700c08" tabindex="-1" value=""></div>
                       <div class="clear">
-                        <input
-                          type="submit"
-                          value="Subscribe"
-                          name="subscribe"
-                          id="mc-embedded-subscribe"
-                          class="button"
-                        />
-                      </div>
+                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                        </div>
                     </div>
                   </form>
                 </div>
-                <script
-                  type="text/javascript"
-                  src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
-                ></script>
-                <script type="text/javascript">
-                  (function ($) {
-                    window.fnames = new Array();
-                    window.ftypes = new Array();
-                    fnames[1] = "FNAME";
-                    ftypes[1] = "text";
-                    fnames[0] = "EMAIL";
-                    ftypes[0] = "email";
-                  })(jQuery);
-                  var $mcj = jQuery.noConflict(true);
-                </script>
                 <!--End mc_embed_signup-->
               </div>
             </div>
