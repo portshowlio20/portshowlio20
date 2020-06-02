5644fb3d1cb6be532fc3b4fcbfc29aa0268ed52a
Martin <martinthelindberg@gmail.com>
2020-06-02 08:13:34 -0700

    maskSwap.js only on author page

--- functions/scripts/student-scripts.php
+++ functions/scripts/student-scripts.php
@@ -4,11 +4,13 @@ add_action('wp_enqueue_scripts', 'add_mask_swap_script');
 function add_mask_swap_script()
 {
   // TODO: only apply if page = author.php
-  wp_enqueue_script(
-    'mask_swap', // name your script so that you can attach other scripts and de-register, etc.
-    get_template_directory_uri() . '/js/student/maskSwap.js', // this is the location of your script file
-    ['jquery'], // this array lists the scripts upon which your script depends
-    null,
-    true
-  );
+  if (is_author()) {
+    wp_enqueue_script(
+      'mask_swap', // name your script so that you can attach other scripts and de-register, etc.
+      get_template_directory_uri() . '/js/student/maskSwap.js', // this is the location of your script file
+      ['jquery'], // this array lists the scripts upon which your script depends
+      null,
+      true
+    );
+  }
 }
