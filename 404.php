86f0f2ddc6245997ff715f02343b854d82250a12
Martin <martinthelindberg@gmail.com>
2020-06-12 13:12:35 -0700

    404 page

--- 404.php
+++ 404.php
@@ -7,54 +7,22 @@
  * @package portshowlio20
  */
 
-get_header();
-?>
+get_header(); ?>
 
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'portshowlio20' ); ?></h1>
-			</header><!-- .page-header -->
+      <div class="ticker-404"></div>
+      <div class="container">
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'portshowlio20' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'portshowlio20' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$portshowlio20_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'portshowlio20' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$portshowlio20_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+        <div class="error-msg">
+          <h2 class="headline">Error 404</h2>
+          <p class="subhead">You've entered an empty sector.</p>
+        </div>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
 
-<?php
-get_footer();
+<?php get_footer();
